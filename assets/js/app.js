c2b27ded84fa7565f24bc720f7e5419589bb83b5
enochwu <enochwu7@gmail.com>
2017-12-12 14:20:43 -0800

    JS Update
    Code to stop and start GIFs is added and functional. API Key has also been changed. this app is mostly done with some fine tuning to come in the frontend and possibly some code to make displayed results... PG.

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
 
     var animalName = $(this).attr("data-name");
     console.log(animalName);
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animalName + "&api_key=dc6zaTOxFJmzC&limit=12";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animalName + "&api_key=Xsvjyjb7Jzs4h7HiMXAAOTjGJn30uB3b&limit=12";
     console.log(queryURL);
 
     $('#gifSpace').empty();
@@ -44,14 +44,36 @@ $(document).ready(function() {
         var animalPic = $("<img />");
 
         // Setting the src attribute of the image to a property pulled off the result item
+        animalPic.addClass('animalPic img-rounded media-middle');
         animalPic.attr("src", results[i].images.fixed_height.url);
-        animalPic.addClass('animalPic img-rounded media-middle')
+        animalPic.attr("data-animate", results[i].images.fixed_height.url);
+        animalPic.attr("data-still", results[i].images.fixed_height_still.url);
+        animalPic.attr("data-still", results[i].images.fixed_height_still.url);
+        animalPic.attr("data-state", "animate");
+
         // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
         $("#gifSpace").prepend(animalPic);
       }
     });
 
+  });
 
+  $(document).on("click", ".animalPic", function() {
+    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+    var state = $(this).attr("data-state");
+    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+    // Then, set the image's data-state to animate
+    // Else set src to the data-still value
+    if (state === "animate") {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    } else {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    }
   });
 
 });
+
+//Ask Mark about filtering PG. Can we filter out via ratings?
+//Mebe some novelty fonts for some added fun.
